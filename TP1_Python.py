2383301dcfcb10df3f54a4dd10b28e78a6a18904
Octavio Ordaz <octavio.ordaz13@gmail.com>
2018-08-21 22:14:36 -0500

    TrabajoPractico1
    Soluciones al TP1 de Python

---
+++ TP1_Python.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 21 16:54:46 2018
+
+@author: usuario
+"""
+
+#1.a Funcion para agregar ceros a un arreglo
+def desplazaArreglo(a,k):
+    aux = []
+    if k > 0:
+        for i in range(k):
+            aux.append(0)
+        a[0:k] = []
+        for j in range(len(a)):
+            aux.append(a[j])
+        a = aux
+    else:
+        k = abs(k)
+        a[len(a)-k:len(a)] = []
+        for i in range(len(a)):
+            aux.append(a[i])
+        a = aux
+        for j in range(k):
+            a.append(0)
+    return a
+#1.b Funcion de la suma de matrices
+def sumaMatrices(a,b):
+    if len(a) == len(b):
+        c = [[0]*len(a) for i in range(len(a))]
+        for i in range(len(a)):
+           for j in range(len(a[0])):
+               c[i][j] = a[i][j] + b[i][j]
+        return c
+    else:
+        return "No se puede realizar"
+#2. Funcion del promedio ponderado
+def promedioPond(calif):
+    pond = [.10, .20, .23, .30, .17]
+    prom = 0
+    for i in range(len(calif)):
+        if calif[i] >= 6:
+            prom = prom + calif[i]*pond[i]
+    return round(prom)
+#3. Funcion que obtiene los pares de una lista
+def tuplaPares(a):
+    resp = []
+    for i in range(0, len(a), 2):
+        resp.append(a[i])
+    return resp
+#4. Funcion que verifica si hay elementos iguales
+def superposicion(a,b):
+    bandera = False
+    for i in range(len(a)):
+        if bandera == False:
+            for j in range(len(b)):
+                if bandera == False:
+                    bandera = a[i] == b[j]
+                else:
+                    break
+        else:
+            break
+    return bandera
+                        
+
+#Prueba de la funcion desplazaArreglo
+a = [1,2,3,4,5,6]
+k = -3
+resp = desplazaArreglo(a,k)
+#Prueba de la funcion del promedio ponderado
+import random
+calif = []
+for i in range(5):
+    calif.append(random.randint(6,10))
+prom = promedioPond(calif)
+#Prueba suma de matrices
+x = [[1,2,3], [4,5,6], [7 ,8,9]]
+y = [[9,8,7], [6,5,4], [3,2,1]]
+resp = sumaMatrices(x,y)
+#Prueba de la funcion tuplaPares
+b = ['a','b','c','d']
+resp = tuplaPares(b)
+#Prueba superposicion
+m = ['a','b','c','d']
+n = [1,2,3,4,5,6]
+resp = superposicion(m,n)
