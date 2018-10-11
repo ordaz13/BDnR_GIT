5b859caac93ad2a191f58f5c08ee111073750337
Octavio Ordaz <octavio.ordaz13@gmail.com>
2018-10-11 14:09:15 -0500

    Cambio en colores
    Final

--- PrimerProyecto/caritasR.R
+++ PrimerProyecto/caritasR.R
@@ -3,7 +3,7 @@ datos <- read.csv('datos.csv')
 library(aplpack)
 faces(datos[,3:7])
 
-plotdat <- matrix(3,nrow=31,ncol=15) # all constant
+plotdat <- matrix(runif(15*31),nrow=31,ncol=15) # all constant
 #rownames(plotdat)<- c("Distancia","Horas", "Genero", "Edad", "Viajes")
 # now set only the columns aka face components of interest
 #plotdat[,2]<-datos$Colonia
