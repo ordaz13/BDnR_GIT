09a155ff102f5ba84236557c4c652b6e35d6dadd
Octavio Ordaz <octavio.ordaz13@gmail.com>
2018-10-11 18:21:03 -0500

    Version final
    Colores ya finales

--- PrimerProyecto/caritasR.R
+++ PrimerProyecto/caritasR.R
@@ -1,15 +1,13 @@
 #install.packages("aplpack")
 datos <- read.csv('datos.csv')
-library(aplpack)
-faces(datos[,3:7])
 
-plotdat <- matrix(runif(15*31),nrow=31,ncol=15) # all constant
+plotdat <- matrix(1,nrow=31,ncol=15) # all constant
 #rownames(plotdat)<- c("Distancia","Horas", "Genero", "Edad", "Viajes")
 # now set only the columns aka face components of interest
 #plotdat[,2]<-datos$Colonia
-plotdat[,3]<-datos$Distancia
-plotdat[,4]<-datos$Horas
-plotdat[,5]<-datos$Genero
+plotdat[,1]<-datos$Distancia
+plotdat[,11]<-datos$Horas
+plotdat[,9]<-datos$Genero
 plotdat[,6]<-datos$Edad
 plotdat[,7]<-datos$Viajes
 
